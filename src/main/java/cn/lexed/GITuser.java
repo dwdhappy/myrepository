b55ae2f1f6ec978ee5f3ea6def6ceec7c931c7c6
abc <18936669368@163.com>
2021-08-25 14:21:02 +0800

    主干开发

--- src/main/java/cn/lexed/GITuser.java
+++ src/main/java/cn/lexed/GITuser.java
@@ -4,7 +4,13 @@ public class GITuser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("userit!");
+		System.out.println("xiugu");
+		System.out.println("xiugu");
+		System.out.println("xiugu");
+		System.out.println("main");
+	
 	}
 
 }
